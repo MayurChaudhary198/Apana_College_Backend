c16071a47b6c839d6d69a2ac18e658bdb3b1a494
MayurChaudhary198 <chaudharymayur198@gmail.com>
2025-01-27 00:12:28 +0530

    Express practice

---
+++ ExpressDir/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+let port = 8080;
+
+app.listen(port , () => {
+    console.log(`app is listening on port ${port}`);
+    
+});
+
+app.get("/", (req ,res)=>{
+    res.send("hii i am root");
+})
+
+// app.get("/:username", (req ,res)=>{
+//     let {username} = req.params;
+//     res.send(`hii @${username}.`);
+// })
+
+app.get("/search", (req ,res)=>{
+    console.log(req.query);
+    res.send("sucess");
+})
