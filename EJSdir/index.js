cea62d8541086bda6f403429f065a8f6981b0559
MayurChaudhary198 <chaudharymayur198@gmail.com>
2025-01-30 18:25:50 +0530

    EJS  practice

--- EJSdir/index.js
+++ EJSdir/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 8080;
 
+// app.use(express.static("public"))
 app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname,"/views"));
-
+app.set("views", path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"public/js")));
+app.use(express.static(path.join(__dirname,"public/css")));
 app.get("/",(req,res) => {
     res.render("home.ejs")
 })
@@ -16,7 +19,15 @@ app.get("/rolldice",(req,res) => {
 
 app.get("/ig/:username" ,(req,res) => {
     let { username } = req.params;
-    res.render("instagram.ejs",{username})
+    const instaData = require("./data.json")
+    const data=instaData[username] 
+    if(data){
+        res.render("instagram.ejs",{data})
+    }
+    else{
+        res.render("error.ejs")
+    }
+    
 })
 
 app.listen(port ,() =>{
